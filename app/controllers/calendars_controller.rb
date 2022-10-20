6b3413c612b4ef9ad3230a075857eccbbefdc92d
masafumiikeda <244marshall144@gmail.com>
2022-10-20 14:48:10 +0900

    コンフリクト修正03

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,5 @@
 class CalendarsController < ApplicationController
   before_action :authenticate_user!
-
   def index
     @events = Event.all 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_090403) do
+ActiveRecord::Schema.define(version: 2022_10_20_085834) do
 
-  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "start_time"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_10_13_090403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
